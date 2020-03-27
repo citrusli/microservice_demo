f0215795021a1117c6fc0406edb222c817a3d092
citrusli <15927162537@163.com>
2020-03-27 12:00:14 +0800

    initial commit

---
+++ demo_commons_domain/src/main/java/com/whut/demo/commons/domain/Tb.java
@@ -0,0 +1,40 @@
+package com.whut.demo.commons.domain;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "demo..tb")
+public class Tb {
+    @Id
+    private Integer no;
+
+    private String name;
+
+    /**
+     * @return no
+     */
+    public Integer getNo() {
+        return no;
+    }
+
+    /**
+     * @param no
+     */
+    public void setNo(Integer no) {
+        this.no = no;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
